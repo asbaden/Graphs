7d4bde4d1e9552c3f6963626cce882ed12e56ba5
asbaden <gusbaden96@gmail.com>
2020-06-12 16:55:07 -0700

    DFS helper funciton created, part 1 of dft plan created

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,9 +1,9 @@
 from room import Room
 from player import Player
 from world import World
-
-import random
+from util import Stack
 from ast import literal_eval
+
 
 # Load world
 world = World()
@@ -30,11 +30,44 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
+#`player.current_room.id` will return the current room, `player.current_room.get_exits()` will show rthe available neighbors, and `player.travel(direction)` will move the player 
+# edges will the dictionary get exits provides with the av
+# To iterate through the list in a dft function  create a helper function that uses the "travel" method from player to move in a direction
+# similar to the traversal test below, add that direction of that move to the traversal path to keep track of the moves 
+
+# create a dictionary called backtrack with the swaped values of direciton to move in the opposite direction 
+
+# Use a DFS function that utilizes back tracking with visited and move as arguments set to none
+    # set the current room to the player.current_room.id
+    # create the edges with setting neighbors to player.current_room.get_exits()
+    # create a conditional checking if no nodes have been visited, create a set(dictionary) to store the nodes we visit
+
+
+
+
+
+
+# Movement in the dft function
+
+def DFS_util(direction):
+    player.travel(direction)
+    traversal_path.append(direction)
+    print("this is traversal path", traversal_path)
+
+# print("this is current room", player.current_room.id)
+
+print("this is testing uutil", DFS_util("n"))
+
+
+
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
+
+#visited rooms counted with current room on each move 
 
 for move in traversal_path:
     player.travel(move)
